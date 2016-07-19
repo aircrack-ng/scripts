5370389fcc9a73e424a98e6c033671483dfd3049
Mister X <tdotreppe@aircrack-ng.org>
2016-07-18 19:35:05 -0600

    Improved sync script.
    Added comment, verify parameters exist and copy UUID.

--- server/svnsync-init.sh
+++ server/svnsync-init.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
+if [ -z "$2" ]; then
+	echo "Missing parameters."
+	echo "$0 <LOCAL_DIR> <URL_TO_SYNC>"
+	exit 0
+fi
+
+# Create repo
 svnadmin create $1
+
+# Copy UUID from other
+REPO_UUID=$(svn info $2 2>/dev/null | grep UUID | awk '{print $3}')
+if [ -z "${REPO_UUID}" ]; then
+	echo "Can't get UUID from remote repo, is it up?"
+	exit 1
+fi
+svnadmin setuuid $1 ${REPO_UUID}
+
+# Prepare for sync
 echo '#!/bin/bash' > $1/hooks/pre-revprop-change
 chmod +x $1/hooks/pre-revprop-change
+
+# Sync
 svnsync init file://${PWD}/$1 $2
 svnsync sync file://${PWD}/$1
+
+# Set permission
+chown -R www-data.www-data $1
+
 echo DONE
